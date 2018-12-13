221232fe4c5b39dcf2a9c95c0f74769d7a3c2eb1
Moey Design <5884seiya@gmail.com>
2018-12-13 12:18:17 -0500

    added remove-todo function

--- todo/plain/main.js
+++ todo/plain/main.js
@@ -2,26 +2,56 @@ var todos = [];
 var todoInputElement;
 var todoListElement;
 
+// called when document is loaded
 window.onload = function() {
   // get DOM elements
   todoInputElement = document.getElementById('add-todo-textinput');
   todoListElement = document.getElementById('todo-list');
 
   // add onclick event listener to the button
-  document.querySelector('.add-todo-container button').onclick = onButtonClicked;
+  var addButtonElement = document.querySelector('.add-todo-container button.add');
+  addButtonElement.addEventListener('click', onAddButtonClicked);
 };
 
-function onButtonClicked() {
+// called when ADD button is clicked
+function onAddButtonClicked() {
   // get the todo text
   var todo = todoInputElement.value;
   if (!todo) return;
 
-  // add a list element with the todo text to the todo list
+  // add a list element with the todo text to the todo list.
+  // The element is going to look like:
+  // <li class="todo-list-item">
+  //   <span>{{ todo }}</span>
+  //   <button
+  //     type="button"
+  //     class="done"
+  //     onclick="onDoneButtonClick">
+  //     DONE!
+  //   </button>
+  // </li>
   var li = document.createElement('li');
-  var text = document.createTextNode(todo);
-  li.appendChild(text);
+  li.classList.add('todo-list-item');
+  li.setAttribute('data-index', todoListElement.childElementCount);
+  var span = document.createElement('span');
+  span.innerHTML = todo;
+  var button = document.createElement('button');
+  button.classList.add('done');
+  button.setAttribute('type', button);
+  button.addEventListener('click', onDoneButtonClicked);
+  button.innerHTML = 'DONE!';
+  li.appendChild(span);
+  li.appendChild(button);
   todoListElement.appendChild(li);
 
-  // reset the text value
+  // clear the text value
   todoInputElement.value = '';
+}
+
+// called when DONE! button is clicked
+function onDoneButtonClicked(event) {
+  // remove the todo item whose DONE! button is clicked
+  var targetIndex = event.target.parentElement.dataset.index;
+  var targetElement = todoListElement.querySelector('li[data-index="' + targetIndex + '"]');
+  todoListElement.removeChild(targetElement);
 }
