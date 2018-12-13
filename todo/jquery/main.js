59e5f5b48fa4f5dd6f9f7bdde42a8714741f3d01
Moey Design <5884seiya@gmail.com>
2018-12-13 12:44:59 -0500

    added remove-todo function

--- todo/jquery/main.js
+++ todo/jquery/main.js
@@ -2,24 +2,55 @@ var todos = [];
 var $todoInputElement;
 var $todoListElement;
 
+// called when document is loaded
 $(function() {
   // get DOM elements
   $todoInputElement = $('#add-todo-textinput');
   $todoListElement = $('#todo-list');
 
   // add onclick event listener to the button
-  $('.add-todo-container button').click(onButtonClicked);
+  $('.add-todo-container button.add').click(onAddButtonClicked);
 });
 
-function onButtonClicked() {
+// called when ADD button is clicked
+function onAddButtonClicked() {
   // get the todo text
   var todo = $todoInputElement.val();
   if (!todo) return;
 
-  // add a list element with the todo text to the todo list
-  var li = $('<li></li>').text(todo);
-  $todoListElement.append(li);
+  // add a list element with the todo text to the todo list.
+  // The element is going to look like:
+  // <li class="todo-list-item">
+  //   <span>{{ todo }}</span>
+  //   <button
+  //     type="button"
+  //     class="done"
+  //     onclick="onDoneButtonClick">
+  //     DONE!
+  //   </button>
+  // </li>
+  var $li = $('<li>', {
+    class: 'todo-list-item',
+    'data-index': $todoListElement.children().length
+  });
+  var $span = $('<span></span>').text(todo);
+  var $button = $('<button></button', {
+    class: 'done',
+    type: 'button',
+  }).text('DONE!');
+  $button.click(onDoneButtonClicked);
+  $li.append($span);
+  $li.append($button);
+  $todoListElement.append($li);
 
   // reset the text value
   $todoInputElement.val('');
+}
+
+// called when DONE! button is clicked
+function onDoneButtonClicked(event) {
+  // remove the todo item whose DONE! button is clicked
+  var targetIndex = event.target.parentElement.dataset.index;
+  var $targetElement = $todoListElement.find('li[data-index="' + targetIndex + '"]');
+  $targetElement.remove();
 }
